0c3d08349c67f6fcb1e447defa4e5f2accd37b6f
Eris <221724216+telly-del@users.noreply.github.com>
2026-06-10 20:45:03 -0700

    Finish project

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+console.log("Play rock paper scissors against the computer! First to 5 wins!");
+
 function getComputerChoice(){
     let choice = Math.floor(Math.random() * 3) + 1;
     console.log(choice);
@@ -23,66 +25,82 @@ function getUserChoice(){
 // If value of users choice is rock and computers value choice is scissors, return console.log("Rock beats scissors! You win!")
 // If the value of users 
 
-function playRound(uc, cc){
-    let playerWin = null;
-    let matchDraw = false;
-    if (uc === "Rock" || uc === "rock" || uc === "ROCK"){
-        if (cc === "rock") {
-            console.log("Rock on Rock is a draw!");
-            matchDraw = true;
+function playGame(){
+    const userSelection = getUserChoice();
+    const computerSelection = getComputerChoice();
+    playRound(userSelection, computerSelection);
+    function playRound(uc, cc){
+        let playerWin = false;
+        let matchDraw = false;
+        let computerWin = false;
+        if (uc === "Rock" || uc === "rock" || uc === "ROCK"){
+            if (cc === "rock") {
+                alert("Rock on Rock is a draw!")
+                matchDraw = true;
+            }
+            if (cc === "paper") {
+                alert("Paper beats Rock!");
+                playerWin = false;
+                computerWin = true;
+            }
+            if (cc === "scissors") {
+                alert(`Rock beats Scissors!`);
+                playerWin = true;
+            }
         }
-        if (cc === "paper") {
-            console.log("Paper beats Rock! You lose!");
-            playerWin = false;
+        if (uc === "Paper" || uc === "paper" || uc === "PAPER"){
+            if (cc === "rock") {
+                alert("Paper beats Rock!");
+                playerWin = true;
+            }
+            if (cc === "paper") {
+                alert("Paper on Paper is a draw!");
+                matchDraw = true;
+            }
+            if (cc === "scissors") {
+                alert("Scissors beats Paper!");
+                playerWin = false;
+                computerWin = true;
+            }
         }
-        if (cc === "scissors") {
-            console.log("Rock beats Scissors! You win!");
-            playerWin = true;
+        if (uc === "Scissors" || uc === "scissors" || uc === "SCISSORS"){
+            if (cc === "rock") {
+                alert("Rock beats Scissors!");
+                playerWin = false;
+                computerWin = true;
+            }
+            if (cc === "paper") {
+                alert("Scissors beats Paper!");
+                playerWin = true;
+            }
+            if (cc === "scissors") {
+                alert("Scissors on scissors is a draw!");
+                matchDraw = true;
+            }
         }
-    }
-    if (uc === "Paper" || uc === "paper" || uc === "PAPER"){
-        if (cc === "rock") {
-            console.log("Paper beats Rock! You win!");
-            playerWin = true;
-        }
-        if (cc === "paper") {
-            console.log("Paper on Paper is a draw!");
-            matchDraw = true;
-        }
-        if (cc === "scissors") {
-            console.log("Scissors beats Paper! You lose!");
-            playerWin = false;
-        }
-    }
-    if (uc === "Scissors" || uc === "scissors" || uc === "SCISSORS"){
-        if (cc === "rock") {
-            console.log("Rock beats Scissors! You lose!");
-            playerWin = false;
-        }
-        if (cc === "paper") {
-            console.log("Scissors beats Paper! You win!");
-            playerWin = true;
-        }
-        if (cc === "scissors") {
-            console.log("Scissors on scissors is a draw!");
-            matchDraw = true;
-        }
-    }
 
-    if (playerWin === true){
-        return ++userScore;
-    } else{
-        return ++computerScore;
+        if (playerWin === true && !matchDraw){
+            return ++userScore;
+        } else if (computerWin === true && !matchDraw){
+            return ++computerScore;
+        }
     }
 }
-const userSelection = getUserChoice();
-const computerSelection = getComputerChoice();
-
 let userScore = 0;
-
 let computerScore = 0;
+playGame();
+
+playGame();
+
+playGame();
+
+playGame();
+ 
+playGame();
 
 
-playRound(userSelection, computerSelection);
-console.log(userScore);
-console.log(computerScore);
+if (userScore > computerScore){
+        console.log("You won the game!")
+} else {
+        console.log("The computer won the game! You lose!")
+}
